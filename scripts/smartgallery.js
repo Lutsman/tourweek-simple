08ff3da962992edf94c56811326d264c600947e4
AdminingSS <admining@mail.ru>
2019-07-18 13:57:17 +0300

    photo resizer release v1.0

--- scripts/smartgallery.js
+++ scripts/smartgallery.js
@@ -7,39 +7,6 @@ class SmartGallery {
         this.responsiveWidth = 640;
 
         this.init();
-        //this.reInit();
-    }
-
-    markRows(markClass) {
-        let lastTop = -1;
-        let currentTop = 0;
-        this.photoCollection.each(function () {
-            currentTop = $(this).position().top;
-            if (currentTop !== lastTop) {
-                $(this).addClass(markClass);
-                lastTop = currentTop;
-            }
-        })
-    }
-
-    checkResponsive(breakpoint) {
-        return window.innerWidth < breakpoint;
-    }
-
-    resetAll() {
-
-        this.indexRows = [];
-        this.rowWidths = [];
-        this.photoWidths = [];
-
-        this.photoCollection.attr('style','');
-        this.photoCollection.removeClass('tm-photo-img-wrap');
-        this.photoCollection.addClass('tm-photo-img-cover');
-        this.photoCollection.removeClass('tm-row-start');
-
-        this.markRows('tm-row-start');
-
-        this.photoRowStart = this.photoHolder.find('.tm-photo-img-cover.tm-row-start');
     }
 
     init() {
@@ -73,6 +40,14 @@ class SmartGallery {
 
         this.photoRowStart = this.photoHolder.find('.tm-photo-img-cover.tm-row-start');
 
+        this.main();
+
+        this.makeResponsive();
+
+        $(window).on('resize', this.reInit.bind(this));
+    }
+
+    main() {
         this.holderWidth = this.photoHolder.width();
 
         this.photoRowStart.each((index) => {
@@ -96,7 +71,6 @@ class SmartGallery {
 
             const widthCoefficient = (this.holderWidth - marginsWidth) / lengthsPhoto;
 
-
             for (let i = firstPhoto; i < lastPhoto; i++) {
                 this.photoWidths.push(this.photoCollection.eq(i).width());
                 const newWidth = this.photoWidths[i] * widthCoefficient;
@@ -104,59 +78,18 @@ class SmartGallery {
                 this.photoCollection.eq(i).addClass('tm-photo-img-wrap');
                 this.photoCollection.eq(i).width(newWidth);
             }
-
         }
-
-        if (this.checkResponsive(this.responsiveWidth)) {
-            // this.photoCollection.addClass('tm-photo-force-responsive');
-            this.desctopHolder.hide();
-            this.responsiveHolder.show();
-        }
-        else {
-            this.desctopHolder.show();
-            this.responsiveHolder.hide();
-        }
-
-        $(window).on('resize', this.reInit.bind(this));
     }
 
     reInit() {
         this.resetAll();
 
-        this.holderWidth = this.photoHolder.width();
+        this.main();
 
-        this.photoRowStart.each((index) => {
-            this.indexRows.push(this.photoRowStart.eq(index).index());
-        });
+        this.makeResponsive();
+    }
 
-        for (let j = 0; j < this.indexRows.length; j++) {
-
-            let firstPhoto = this.indexRows[j];
-            let lastPhoto = this.indexRows[j + 1] || this.photoCollection.length;
-
-            let lengthsPhoto = 0;
-            const countPhoto = lastPhoto - firstPhoto;
-            const marginsWidth = countPhoto * 3; //if margin 3px;
-
-            for (let i = firstPhoto; i < lastPhoto; i++) {
-                lengthsPhoto += this.photoCollection.eq(i).width();
-            }
-
-            this.rowWidths.push(lengthsPhoto);
-
-            const widthCoefficient = (this.holderWidth - marginsWidth) / lengthsPhoto;
-
-
-            for (let i = firstPhoto; i < lastPhoto; i++) {
-                this.photoWidths.push(this.photoCollection.eq(i).width());
-                const newWidth = this.photoWidths[i] * widthCoefficient;
-                this.photoCollection.eq(i).removeClass('tm-photo-img-cover');
-                this.photoCollection.eq(i).addClass('tm-photo-img-wrap');
-                this.photoCollection.eq(i).width(newWidth);
-            }
-
-        }
-
+    makeResponsive() {
         if (this.checkResponsive(this.responsiveWidth)) {
             this.desctopHolder.hide();
             this.responsiveHolder.show();
@@ -165,41 +98,37 @@ class SmartGallery {
             this.desctopHolder.show();
             this.responsiveHolder.hide();
         }
-
-        // if (this.checkResponsive(this.responsiveWidth)) {
-        //     this.photoCollection.addClass('tm-photo-force-responsive');
-        // }
-        // else {
-        //     this.photoCollection.removeClass('tm-photo-force-responsive');
-        // }
     }
-    //     this.holderWidth = this.photoHolder.width();
-    //
-    //     for (let j = 0; j < this.indexRows.length; j++) {
-    //
-    //         let firstPhoto = this.indexRows[j];
-    //         let lastPhoto = this.indexRows[j + 1] || this.photoCollection.length;
-    //
-    //         const countPhoto = lastPhoto - firstPhoto;
-    //         const marginsWidth = countPhoto * 3; //if margin 3px;
-    //
-    //         const widthCoefficient = (this.holderWidth - marginsWidth) / this.rowWidths[j];
-    //
-    //
-    //         for (let i = firstPhoto; i < lastPhoto; i++) {
-    //             const newWidth = this.photoWidths[i] * widthCoefficient;
-    //             this.photoCollection.eq(i).width(newWidth);
-    //
-    //         }
-    //
-    //     }
-    //
-    //     if (this.checkResponsive(this.responsiveWidth)) {
-    //         this.photoCollection.addClass('tm-photo-force-responsive');
-    //     }
-    //     else {
-    //         this.photoCollection.removeClass('tm-photo-force-responsive');
-    //     }
-    // }
 
+    checkResponsive(breakpoint) {
+        return window.innerWidth < breakpoint;
+    }
+
+    markRows(markClass) {
+        let lastTop = -1;
+        let currentTop = 0;
+        this.photoCollection.each(function () {
+            currentTop = $(this).position().top;
+            if (currentTop !== lastTop) {
+                $(this).addClass(markClass);
+                lastTop = currentTop;
+            }
+        })
+    }
+
+    resetAll() {
+
+        this.indexRows = [];
+        this.rowWidths = [];
+        this.photoWidths = [];
+
+        this.photoCollection.attr('style','');
+        this.photoCollection.removeClass('tm-photo-img-wrap');
+        this.photoCollection.addClass('tm-photo-img-cover');
+        this.photoCollection.removeClass('tm-row-start');
+
+        this.markRows('tm-row-start');
+
+        this.photoRowStart = this.photoHolder.find('.tm-photo-img-cover.tm-row-start');
+    }
 }
